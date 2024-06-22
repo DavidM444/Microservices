475fe2e2dd93d0a58025630df146e1f50d246cf2
DavidM444 <david541gamer@gmail.com>
2024-06-22 18:58:28 -0500

    User entity refactor.

--- Producer/src/main/java/com/kafka/producer/Entity/User.java
+++ Producer/src/main/java/com/kafka/producer/Entity/User.java
@@ -1,16 +1,32 @@
 package com.kafka.producer.Entity;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.UUID;
-
-@Builder
-@Data
+@Getter
 public class User {
-
-    private UUID id;
+    private UUID id = UUID.randomUUID();
     private String nombre;
     private String email;
     private String edad;
+
+    public User(UUID id,String nombre, String email, String edad){
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", edad='" + edad + '\'' +
+                '}';
+    }
 }
