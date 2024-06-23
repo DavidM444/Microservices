5fc0d3a5937d5b47d6c20efa85c4400f6d8960b3
DavidM444 <david541gamer@gmail.com>
2024-06-22 19:00:53 -0500

    Kafka consumer configuration with json convert.

---
+++ consumer/src/main/java/com/kafka/consumer/App/KafkaConsumerConfig.java
@@ -0,0 +1,43 @@
+package com.kafka.consumer.App;
+
+import kafka.producer.Entity.User;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, User> consumerFactory() {
+        Map<String, Object> prop = new HashMap<>();
+        prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        prop.put(ConsumerConfig.GROUP_ID_CONFIG, "user-register");
+        prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        prop.put(JsonDeserializer.TRUSTED_PACKAGES,"com.kafka.producer.Entity");
+        return new DefaultKafkaConsumerFactory<>(prop, new StringDeserializer(), new JsonDeserializer<>(User.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> concurrentListener (){
+        ConcurrentKafkaListenerContainerFactory<String, User> conf = new ConcurrentKafkaListenerContainerFactory<>();
+        conf.setConsumerFactory(consumerFactory());
+        return conf;
+    }
+
+    //bean to deserialize request to user class.
+    @Bean
+    public StringJsonMessageConverter stringJsonMessageConverter() {
+        return new StringJsonMessageConverter();
+    }
+}
